1148ef8ec7222e7159fc7e8290d7badf3fa6e7e2
Katja <you@example.com>
2024-09-26 10:10:37 +0300

    Pievienoju virsrakstu un 2 apakšvirsrakstus

---
+++ web/blog.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dienasgrāmata</title>
+    <h1>Tiešsaistes Dienasgrāmata</h1>
+    <h2>Biogrāfija</h2>
+    <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+</head>
+<body>
+    
+</body>
+</html>
