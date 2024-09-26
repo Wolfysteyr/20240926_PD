3d2a4d4f864b85dc330bf164e23b0cce0d7ff741
Katja <you@example.com>
2024-09-26 10:18:10 +0300

    blog.php failā pievienoju lorem100 paragrāfu zem biogrāfija un zem otrā apakšvirsraksta uzrakstīju saraksu ar iemesliem, kāpēc paliku par programmētāju

--- web/blog.php
+++ web/blog.php
@@ -5,10 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dienasgrāmata</title>
     <h1>Tiešsaistes Dienasgrāmata</h1>
-    <h2>Biogrāfija</h2>
-    <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
 </head>
 <body>
+    <div class="biografija">
+        <h2>Biogrāfija</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error temporibus, labore eveniet, officia voluptatum tempore laudantium facilis praesentium possimus, ratione deserunt mollitia doloribus illum inventore commodi eaque. Ipsum earum consectetur animi sint quam laboriosam illum aut veritatis recusandae ullam repellat, maiores voluptatibus? Aliquam veniam ex natus. Autem aperiam ipsum soluta sequi illo numquam magnam? Esse amet voluptates recusandae nemo, quis nam corporis, facere provident adipisci quo voluptas quae saepe odio, nostrum cum deserunt sint culpa. Adipisci id quo, totam molestiae culpa tempore eveniet nulla corrupti fugit officia praesentium aliquid omnis dolorem vitae exercitationem maxime perferendis sunt quaerat dignissimos dolores voluptas.</p>
+
+    </div>
+
+    <br>
+
+    <div class="kapec">
+        <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+        <ul>
+            <li>Jo gribu programmēt</li>
+            <li>Jo gribu naudu</li>
+            <li>Jo man tas patīk</li>
+            <li>Jo ir interesanti</li>
+            <li>Jo ir jautri</li>
+        </ul>
+    </div>
     
 </body>
 </html>
