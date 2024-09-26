1156a70c623d51a34bbc879d82dd66d1735d780a
Katja <you@example.com>
2024-09-26 10:08:56 +0300

    izveidoju index.php un blog.php; index.php failā izveidoju formu ar vārda un email ievada logiem, dropdown izvēlni un radio pogas ar izvēli 'jā' un 'nē'. Pievienoju submit pogu.

---
+++ web/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piesakies webināram!</title>
+    <h1>Piesakies webināram!</h1>
+</head>
+<body>
+    <div class="formstuff">
+        <form method="post">
+            <label for="name">Vārds: </label>
+            <input type="text" name="name" id="name"><br><br>
+
+            <label for="email">E-mail: </label>
+            <input type="text" name="email" id="email"><br><br>
+
+            <label for="select1">Izvēlēties līmeni</label>
+            <select name="select1" id="select1">
+                <option value="Beginner">Beginner</option>
+                <option value="Intermediate">Intermediate</option>
+                <option value="Advanced">Advanced</option>
+            </select>
+        </for>
+
+        
+        <br>
+        <label for="">Vai vēlies saņemt info par citiem mūsu organizētajiem pasākumiem?</label><br>
+        <label for="drone">Jā</label>
+        <input type="radio" name="drone" id="r1">
+        <label for="drone">Nē</label>
+        <input type="radio" name="drone" id="r2">
+        
+        <br>
+        
+        <input type="submit" value="Pieteikties">
+
+    </div>
+</body>
+</html>
