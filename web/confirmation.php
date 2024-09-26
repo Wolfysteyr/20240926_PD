033c9ecc48da334c7d76edf7eeb25b6cfaf4c145
Katja <you@example.com>
2024-09-26 10:50:36 +0300

    uzrakstīju kodu, kurš lasa index.php POST-otu informāciju

---
+++ web/confirmation.php
@@ -0,0 +1,16 @@
+<?php echo ($_POST['name'] . " , Jūs esat veiksmīgi reģistrējusies(-ies) webināram ar e-pastu " . $_POST['email']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirm</title>
+</head>
+<body>
+    <br>
+    
+    <a href="index.php">Back</a>
+</body>
+</html>
